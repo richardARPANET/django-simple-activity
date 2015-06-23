b64e8b1130b73e47826577d0965dcdaa76023f30
Richard O'Dwyer <richard@richard.do>
2015-06-23 08:06:35 +0100

    Changes models to use new location of GenericForeignKey in Django 1.8

--- src/activity/models.py
+++ src/activity/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from django.utils import timezone
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.humanize.templatetags.humanize import naturaltime
 from model_utils.managers import PassThroughManager
 from . querysets import ActionQueryset
@@ -14,7 +14,7 @@ class Action(models.Model):
         ContentType, related_name='actor', blank=True, null=True
     )
     actor_object_id = models.CharField(max_length=500, blank=True, null=True)
-    actor = generic.GenericForeignKey(
+    actor = GenericForeignKey(
         ct_field='actor_content_type', fk_field='actor_object_id'
     )
     # The phrase describing the action of that activity
@@ -25,7 +25,7 @@ class Action(models.Model):
         ContentType, related_name='action', blank=True, null=True
     )
     action_object_id = models.CharField(max_length=500, blank=True, null=True)
-    action = generic.GenericForeignKey(
+    action = GenericForeignKey(
         ct_field='action_content_type', fk_field='action_object_id'
     )
 
@@ -34,7 +34,7 @@ class Action(models.Model):
         ContentType, related_name='target', blank=True, null=True
     )
     target_object_id = models.CharField(max_length=500, blank=True, null=True)
-    target = generic.GenericForeignKey(
+    target = GenericForeignKey(
         ct_field='target_content_type', fk_field='target_object_id'
     )
 
